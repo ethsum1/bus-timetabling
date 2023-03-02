2c46d26c60c1e00bee058a6e63d55c267a2a62e8
ethsum1 <mr.e.sumpter@gmail.com>
2023-03-02 23:24:47 +0000

    Created new functions

--- backend/account-functions.php
+++ backend/account-functions.php
@@ -21,16 +21,20 @@ function pwdMatch($pwd, $pwdRepeat) {
     return ($pwd !== $pwdRepeat);
 }
 
+// Checks the SQL statement is prepared correctly
+function prepareStmt($stmt, $sql) {
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../index.php?error=stmtfailed");
+        exit();
+    } 
+}
+
 // Checks if an email already exists in the database
 function emailExists($conn, $email) {
     $sql = "SELECT * FROM accounts WHERE email = ?;";
     $stmt = mysqli_stmt_init($conn);
 
-    // Checks the SQL statement is prepared correctly
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ../index.php?error=stmtfailed");
-        exit();
-    }
+    prepareStmt($stmt, $sql);
     
     // Binds parameters to the SQL statement
     mysqli_stmt_bind_param($stmt, "s", $email);
@@ -50,27 +54,25 @@ function emailExists($conn, $email) {
 // Creates a new user in the database
 function createUser($conn, $name, $email, $pwd) {
 
+    // Uses prepared statements to prevent SQL injection attacks
     $sql = "INSERT INTO accounts (fname, email, pword) VALUES (?, ?, ?)";
     $stmt = mysqli_stmt_init($conn);
-    if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("location: ../index.php?error=stmtfailed");
-        exit();
-    }
+    prepareStmt($stmt, $sql);
 
+    // Hashes password so it isn't stored in the database in plain text
     $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
-
     mysqli_stmt_bind_param($stmt, "sss", $name, $email, $hashedPwd);
     mysqli_stmt_execute($stmt); 
     mysqli_stmt_close($stmt);
 
     // Finds the user ID of the newly created user
-    $response = emailExists($conn, $email);
+    $userRecord = emailExists($conn, $email);
 
     // Sets the user session variables to the values from the database record
     session_start();
-    $_SESSION["user_fname"] = $response["fname"];
-    $_SESSION["user_email"] = $response["email"];
-    $_SESSION["user_id"] = $response["account_id"];
+    $_SESSION["user_fname"] = $userRecord["fname"];
+    $_SESSION["user_email"] = $userRecord["email"];
+    $_SESSION["user_id"] = $userRecord["account_id"];
     
     header("location: ../user-homepage.php");
     exit();
@@ -78,31 +80,32 @@ function createUser($conn, $name, $email, $pwd) {
 
 // Logs user into their existing account
 function loginUser($conn, $email, $pwd) {
-    $emailExists = emailExists($conn, $email);
+    $userRecord = emailExists($conn, $email);
 
     // Checks that the email entered does exist on the database
-    if ($emailExists === false) {
+    if ($userRecord === false) {
         header("location: ../index.php?error=wronglogin");
         exit();
     }
 
-    $pwdHashed = $emailExists["pword"];
+    $pwdHashed = $userRecord["pword"];
+    // Confirms password enters matches with hashed password in database
     $checkPwd = password_verify($pwd,$pwdHashed);
 
+    // Creates session if passwords match, displays error if passwords don't match
     if ($checkPwd === false) {
         header("location: ../index.php?error=wronglogin");
         exit();
     }
     else if ($checkPwd === true) {
         session_start();
-        $_SESSION["user_id"] = $emailExists["account_id"];
-        $_SESSION["user_email"] = $emailExists["email"];
-        $_SESSION["user_fname"] = $emailExists["fname"];
+        $_SESSION["user_id"] = $userRecord["account_id"];
+        $_SESSION["user_email"] = $userRecord["email"];
+        $_SESSION["user_fname"] = $userRecord["fname"];
 
         header("location: ../user-homepage.php");
         exit();
     }
-
 }
 
 ?>
